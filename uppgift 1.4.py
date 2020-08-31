0a15ad1be27f18e241c47f6efa6f91aef4c8f6f8
hajmat02 <70145900+hajmat02@users.noreply.github.com>
2020-08-31 14:41:46 +0200

    Övning1.4

--- uppgift 1.4.py
+++
@@ -1,4 +0,0 @@
-#uppgift 1.4.py
-namn=input("Vad heter du?")
-print("Hej", namn)
-name=input("Vad heter du i efternamn?")
---
+++ uppgift1.4.py
@@ -0,0 +1,6 @@
+#uppgift 1.4.py
+namn=input("Vad heter du?")
+print("Hej", namn)
+name=input("Vad heter du i efternamn?")
+name=input("Hur mår du?")
+Name=input("Det var ju inge bra")
