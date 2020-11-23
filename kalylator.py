890d58ee97f2d21aa67fdd57eb9e465738b9b993
hajmat02 <70145900+hajmat02@users.noreply.github.com>
2020-11-23 13:09:39 +0100

    menyn

---
+++ kalylator.py
@@ -0,0 +1,8 @@
+
+räknesätt = str(input('Välokmen till miniräknaren, välj ett av följande val [tyck enter för att fortsätta]'))
+print('[1] Räkna med +')
+print('[2] Räkna med -')
+print('[3] Räkna med *')
+print('[4] Räkna med /')
+print('[5] Avsluta')
+
